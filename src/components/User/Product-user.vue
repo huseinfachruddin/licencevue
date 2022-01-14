9def9eff105e8dde5d732d62e656684225f24780
unknown <husein.18@students.amikom.ac.id>
2022-01-14 13:26:46 +0700

    sdfg

--- src/components/User/Product-user.vue
+++ src/components/User/Product-user.vue
@@ -34,7 +34,7 @@
               <v-list-item-title class="text-h5 mb-1">
                 {{ data.name }}
               </v-list-item-title>
-              <v-list-item-subtitle>{{ data.desc }}</v-list-item-subtitle>
+              <v-list-item-subtitle v-html="data.desc"></v-list-item-subtitle>
             </v-list-item-content>
 
             <v-list-item-avatar tile size="80" color="blue">
